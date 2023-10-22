f45f0d5904c3e887e8337ca758d66a8d1e60f27b
4M <mossmost123@gmail.com>
2023-10-23 01:24:34 +0700

    cart api

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,6 +1,33 @@
+import { 
+    Body,  
+    Delete,  
+    Get,  
+    Param,  
+    Post, 
+    UseGuards 
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/register/tools'
 import { Controller } from '@nestjs/common';
+import { CartService } from './cart.service';
+import { GetUser } from 'src/auth/auth.decorator';
 
-@Controller('cart')
+@Controller("cart")
+@UseGuards(JwtAuthGuard)
 export class CartController {
-    
+    constructor(private cartService: CartService) {}
+
+    @Get()
+    getCart(@GetUser('userId') userId: string){
+        return this.cartService.getCart(userId);
+    }
+
+    @Post("/:id")
+    updateCart(@GetUser('userId') userId: string, @Param('id') gameId: string) {
+        return this.cartService.updateCart(userId, gameId);
+    }
+
+    @Delete("/:id")
+    deleteCart(@GetUser('userId') userId: string, @Param('id') gameId: string){
+        return this.cartService.deleteCart(userId, gameId);
+    }
 }
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+    constructor(private prisma: PrismaService) {}
+    
+    async getCart(userId: string){
+        const userInfo = await this.prisma.user.findUnique({
+            where: {
+                userId: userId,
+            },
+        });
+
+        return userInfo.cartedGameIds;
+    }
+
+    async updateCart(userId: string, gameId: string){
+        const userInfo = await this.prisma.user.findUnique({
+            where: {
+                userId: userId,
+            },
+        });
+
+        userInfo.cartedGameIds.push(gameId);
+
+        const updateUserCart = await this.prisma.user.update({
+            where: {
+                userId: userId,
+            },
+            data: {
+                cartedGameIds: userInfo.cartedGameIds,
+            },
+        });
+
+        return updateUserCart;
+    }
+
+    async deleteCart(userId: string, gameId: string){
+        const userInfo = await this.prisma.user.findUnique({
+            where: {
+                userId: userId,
+            },
+        });
+        
+        const updateUserCart = await this.prisma.user.update({
+            where: {
+                userId: userId,
+            },
+            data: {
+                cartedGameIds: userInfo.cartedGameIds.filter((id) => id !== gameId),
+            },
+        });
+
+        return updateUserCart;
+    }
+}
