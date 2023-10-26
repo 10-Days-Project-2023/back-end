110a27070904aeb698f85178bb07eeee93cc0efc
NasmeenI <nasmeen2503@gmail.com>
2023-10-27 00:50:15 +0700

    get random game by member

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -11,21 +11,15 @@ import { JwtAuthGuard } from '../auth/register/tools'
 import { GameService } from './game.service';
 import { CreateGameDto, EditGameDto, GetGameByGenreDto } from './tools';
 import { GetUser } from '../auth/auth.decorator';
+import { User } from '@prisma/client';
 
 @Controller('game')
 export class GameController {
   constructor(private gameService: GameService) {}
 
-  @Post()
-  @UseGuards(JwtAuthGuard)
-  createGame(@GetUser('userId') userId: string, @Body() dto: CreateGameDto) {
-    return this.gameService.createGame(userId, dto);
-  }
-
-  @Patch()
-  @UseGuards(JwtAuthGuard)
-  editGame(@GetUser('userId') userId: string, @Body() dto: EditGameDto) {
-    return this.gameService.editGame(userId, dto);
+  @Get(':id')
+  getGameById(@Param('id') id : string) {
+    return this.gameService.getGameById(id);
   }
 
   @Get('genre')
@@ -43,8 +37,21 @@ export class GameController {
     return this.gameService.getRandomGame();
   }
 
-  @Get(':id')
-  getGameById(@Param('id') id : string) {
-    return this.gameService.getGameById(id);
+  @Get('randomByMember')
+  @UseGuards(JwtAuthGuard)
+  getRandomGameByMember(@GetUser() user: User){
+    return this.gameService.getRandomGameByMember(user);
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  createGame(@GetUser('userId') userId: string, @Body() dto: CreateGameDto) {
+    return this.gameService.createGame(userId, dto);
+  }
+
+  @Patch()
+  @UseGuards(JwtAuthGuard)
+  editGame(@GetUser('userId') userId: string, @Body() dto: EditGameDto) {
+    return this.gameService.editGame(userId, dto);
   }
 }
