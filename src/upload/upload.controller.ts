22be7d1ce94c5eb6e0fc110d9d70e241c8fea50e
4M <mossmost123@gmail.com>
2023-10-29 12:03:43 +0700

    add s3 file

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, FileTypeValidator, HttpStatus, ParseFilePipe, ParseFilePipeBuilder, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, FileTypeValidator, Get, HttpStatus, ParseFilePipe, ParseFilePipeBuilder, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
 import { GetUser } from 'src/auth/auth.decorator';
@@ -9,18 +9,19 @@ import { JwtAuthGuard } from 'src/auth/register/tools';
 export class UploadController {
     constructor(private readonly uploadService: UploadService) {}
 
-    @Post()
-    @UseInterceptors(FileInterceptor('file'))
-    async uploadProfilePic(@UploadedFile(
-        new ParseFilePipeBuilder()
-          .addFileTypeValidator({
-            fileType: /(jpg|jpeg|png|gif)$/,
-          })
-          .addMaxSizeValidator({ maxSize: 5242880 })
-          .build({
-            errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-          }),
-      ) file: Express.Multer.File, @GetUser('userId') userId: string) {
-        await this.uploadService.uploadProfilePic(file.originalname, file.buffer, userId);
+    @Get()
+    async getProfilePic(@GetUser('userId') userId: string) {
+        return this.uploadService.getProfilePic(userId);
     }
+
+    @Post('profilepic')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadFileSubsecMultifile(
+        @UploadedFile() file: Express.Multer.File,
+        @GetUser('userId') userId: string
+    ) {
+        return this.uploadService.uploadProfilePic(userId, file);
+    }
+
+
 }
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { Prisma } from '@prisma/client';
+import AWS, { S3 } from 'aws-sdk';
 
 @Injectable()
 export class UploadService {
@@ -12,18 +13,51 @@ export class UploadService {
 
     constructor(private readonly configService: ConfigService, private prisma: PrismaService) {}
 
+    async uploadProfilePic(userId: string, file: Express.Multer.File) {
+        const userInfo = await this.prisma.user.findUnique({
+            where: {
+                userId: userId,
+            },
+        });
 
-    async uploadProfilePic(fileName: string, file: Buffer, userId: string){
+        const currentTime: Date = new Date();
+        const fullFilePath: string = `10day-profilepic/${userId}`;
 
-        const fullFilePath: string = `10day-profilepic/${fileName}`;
-
-        await this.s3Client.send(
+        const uploadResult: any = await this.s3Client.send(
             new PutObjectCommand({
                 Bucket: process.env.AWS_S3_BUCKET_NAME,
                 Key: fullFilePath,
-                Body: file,
+                Body: file.buffer,
             }),
         );
+       
+        await this.prisma.user.update({
+            where:{
+                userId: userId,
+            },
+            data:{
+                picture: `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${userId}`,
+            },
+        })
+        return console.log('success uploading...');
 
+    }
+
+    async getProfilePic(userId: string){
+        const userInfo = await this.prisma.user.findUnique({
+            where:{userId: userId,},
+        });
+        const profilePicUrl = userInfo.picture;
+        const profilePicKey = profilePicUrl.slice(`https://${this.configService.get<string>('aws.s3_bucket_name')}.s3.${this.configService.get<string>('aws.region')}.amazonaws.com/`.length);
+        const s3 = new AWS.S3();
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: profilePicKey,
+        };
+        const profilePic: any = s3.getObject(params);
+
+        return {
+            profilePicBase64: profilePic.toString('base64')
+        }
     }
 }
